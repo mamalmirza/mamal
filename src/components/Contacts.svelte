503f0cca2c30bae3e0e8e0535773783dc987d231
mamalmirza <59690548+mamalmirza@users.noreply.github.com>
2020-09-04 10:38:17 +0330

    contact links update

--- src/components/Contacts.svelte
+++ src/components/Contacts.svelte
@@ -21,13 +21,13 @@
     <h1>Contact Me</h1>
     <div class="center">
         <div class="box">
-            <a href="">
+            <a href="https://www.linkedin.com/in/mohammad-reza-mirzaei-a9a2b213b">
                 <img src="./icons/Linkedin.svg" alt="Linkedin">
             </a>
-            <a href="">
+            <a href="https://t.me/mamal_mirza">
                 <img src="./icons/Telegram.svg" alt="telegram">
             </a>
-            <a href="">
+            <a href="mailto:Mirzaei.Mohammadreza1997@gmail.com">
                 <img src="./icons/email.svg" alt="email">
             </a>
         </div>
